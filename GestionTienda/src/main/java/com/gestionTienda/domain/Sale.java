06187e88418d76c11a5e7b03d982fabc2f230d0c
Elias Torres <elias.torressn@outlook.com>
2022-08-26 12:45:51 -0300

    add new class 'sold' and its attributes

--- GestionTienda/src/main/java/com/gestionTienda/domain/Sale.java
+++ GestionTienda/src/main/java/com/gestionTienda/domain/Sale.java
@@ -3,7 +3,38 @@
 
 package com.gestionTienda.domain;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import lombok.Data;
+import lombok.Getter;
 
+@Entity
+@Data
+@Table(name="sales")
 public class Sale {
-
+    
+    private static final Long serialVersionUID= 1L;
+    
+    @Id
+    @Getter
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    private long idSold;
+    
+    @NotEmpty
+    @Column(name="art")
+    private String art;
+             
+    @NotEmpty
+    @Column(name="payment")
+    private Enum payment;
+    
+    @NotEmpty
+    @Column(name="sold_tot")
+    private double soldTot;
+             
+    @NotEmpty
+    @Column(name="obs")
+    private String obs;
+                                    
+                                    
 }
